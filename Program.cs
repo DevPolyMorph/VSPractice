0259c02e6c6521bc89f135826e459229c0f37525
Gabriel Anthony Carpio-Pena <gabriel.anthony.pena@gmail.com>
2018-01-18 00:37:48 -0600

    Creation

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VSPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Enter a String!");
+            String input = Console.ReadLine();
+            Console.WriteLine("You entered:");
+            Console.WriteLine(input);
+
+        }
+    }
+}
